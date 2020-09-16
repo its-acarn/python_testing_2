d457bb81beef40685e52c2132c2f07ab72d04fb9
svvishjs <andreww92@live.com>
2020-09-16 17:00:06 +0100

    changed test to pub has drinks list function and created list instances to add drinks

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -9,18 +9,21 @@ class TestPub(unittest.TestCase):
         self.drink_1 = Drink("Tennents", 3.40)
         self.drink_2 = Drink("Wine", 7.50)
 
+        self.pub.add_drink_to_pub(self.drink_1)
+        self.pub.add_drink_to_pub(self.drink_2)
+
     def test_pub_has_name(self):
         self.assertEqual("Ox", self.pub.name)
 
     def test_pub_has_money(self):
         self.assertEqual(100.00, self.pub.till)
 
-    def test_pub_has_empty_drinks_list(self):
-        self.assertEqual(0,len(self.pub.drinks))
+    def test_pub_has_drinks_list(self):
+        self.assertEqual(type([]),type(self.pub.drinks))
 
     def test_pub_has_drinks(self):
-        self.pub.add_drink_to_pub(self.drink_1)
-        self.pub.add_drink_to_pub(self.drink_2)
+        # self.pub.add_drink_to_pub(self.drink_1)
+        # self.pub.add_drink_to_pub(self.drink_2)
         self.assertEqual(2, len(self.pub.drinks))
 
     def test_add_money_to_till(self):
@@ -28,14 +31,14 @@ class TestPub(unittest.TestCase):
         self.assertEqual(107.50, self.pub.till)
 
     def test_remove_drink_from_pub(self):
-        self.pub.add_drink_to_pub(self.drink_1)
-        self.pub.add_drink_to_pub(self.drink_2)
+        # self.pub.add_drink_to_pub(self.drink_1)
+        # self.pub.add_drink_to_pub(self.drink_2)
         self.pub.remove_drink_from_pub(self.drink_1)
         self.assertEqual(1, len(self.pub.drinks))
     
     def test_sell_drink(self):
-        self.pub.add_drink_to_pub(self.drink_1)
-        self.pub.add_drink_to_pub(self.drink_2)
+        # self.pub.add_drink_to_pub(self.drink_1)
+        # self.pub.add_drink_to_pub(self.drink_2)
         self.pub.sell_drink(self.drink_1, self.drink_1.price)
         self.assertEqual(1, len(self.pub.drinks))
         self.assertEqual(103.40, self.pub.till)
