7847c63e39f1d2d8d7db776a1b3b99e8b260f578
svvishjs <andreww92@live.com>
2020-09-17 16:11:18 +0100

    small changes

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -24,8 +24,6 @@ class TestPub(unittest.TestCase):
         self.assertEqual(type([]),type(self.pub.drinks))
 
     def test_pub_has_drinks(self):
-        # self.pub.add_drink_to_pub(self.drink_1)
-        # self.pub.add_drink_to_pub(self.drink_2)
         self.assertEqual(2, len(self.pub.drinks))
 
     def test_add_money_to_till(self):
@@ -33,14 +31,10 @@ class TestPub(unittest.TestCase):
         self.assertEqual(107.50, self.pub.till)
 
     def test_remove_drink_from_pub(self):
-        # self.pub.add_drink_to_pub(self.drink_1)
-        # self.pub.add_drink_to_pub(self.drink_2)
         self.pub.remove_drink_from_pub(self.drink_1)
         self.assertEqual(1, len(self.pub.drinks))
     
     def test_sell_drink(self):
-        # self.pub.add_drink_to_pub(self.drink_1)
-        # self.pub.add_drink_to_pub(self.drink_2)
         self.pub.sell_drink(self.drink_1, self.drink_1.price, self.customer_1.age)
         self.assertEqual(1, len(self.pub.drinks))
         self.assertEqual(103.40, self.pub.till)
